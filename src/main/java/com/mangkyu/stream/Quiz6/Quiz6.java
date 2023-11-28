a5f1f778cb8f58c325399066ed5e88de181f93fb
khs <anjfkrhdkdle@naver.com>
2023-11-28 15:50:47 +0900

    add: 문제6 풀이

--- src/main/java/com/mangkyu/stream/Quiz6/Quiz6.java
+++ src/main/java/com/mangkyu/stream/Quiz6/Quiz6.java
@@ -1,8 +1,11 @@
 package com.mangkyu.stream.Quiz6;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Quiz6 {
 
@@ -33,12 +36,26 @@ public class Quiz6 {
 
     // stuArr에서 불합격(150점 미만)한 학생의 수를 남자와 여자로 구별하여라. (Boolean, List)
     public Map<Boolean, List<Student>> quiz1() {
-        return new HashMap<>();
+        // 초기 풀이
+        // return Arrays.stream(stuArr)
+        //     .filter(student -> student.getScore() < 150)
+        //     .collect(Collectors.toMap(Student::isMale, student->{
+        //         List<Student> students = new LinkedList<>();
+        //         students.add(student);
+        //         return students;
+        //     }, (oldValue, newValue)-> {
+        //         oldValue.addAll(newValue);
+        //         return oldValue;
+        //     }));
+        return Arrays.stream(stuArr)
+            .filter(student -> student.getScore()<150)
+            .collect(Collectors.groupingBy(Student::isMale));
     }
 
     // 각 반별 총점을 학년 별로 나누어 구하여라 (Map<Integer, Map<Integer, Integer>>)
     public Map<Integer, Map<Integer, Integer>> quiz2() {
-        return new HashMap<>();
+        return Arrays.stream(stuArr)
+            .collect(Collectors.groupingBy(Student::getHak, Collectors.groupingBy(Student::getBan, Collectors.summingInt(Student::getScore))));
     }
 
 }
